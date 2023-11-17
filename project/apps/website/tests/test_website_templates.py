a01bc91734ebd0cbeabb2b8845f085b034ec9f03
Henrique Sebastião <contato@henriquesebastiao.com>
2023-11-16 22:55:14 -0400

    Update project

---
+++ project/apps/website/tests/test_website_templates.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class WebsiteTemplatesTest(TestCase):
+    def test_if_template_contain_correct_itens(self):
+        html = self.client.get(reverse("home")).content.decode("utf8")
+        self.assertIn(
+            "Gerencie suas finanças de forma simples e eficiente.", html
+        )
+        self.assertIn(
+            "Poupy é um sistema de controle financeiro pessoal que permite que você\n                gerencie suas finanças de forma simples e eficiente.",
+            html,
+        )
