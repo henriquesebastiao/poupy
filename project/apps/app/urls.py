78f2f80217ca29886d8e663b9459175c07c0a543
Henrique Sebastião <contato@henriquesebastiao.com>
2023-11-18 20:17:06 -0400

    Adding user login and registration screens 💡

--- project/apps/app/urls.py
+++ project/apps/app/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path('', views.app, name='app')]
+urlpatterns = [
+    path('', views.app, name='app'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+]
--- project/apps/app/views.py
+++ project/apps/app/views.py
@@ -1,13 +1,28 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
-from .models import Account, User, Transaction
+from .models import Account, Transaction, User
 
 
 def app(request):
     user = User.objects.get(email='contato@henriquesebastiao.com')
     accounts = Account.objects.filter(user=user)
     total_balance = sum([account.balance for account in accounts])
-    transactions = Transaction.objects.filter(user=user)
+    bigger_transactions_of_month = Transaction.objects.filter(
+        user=user,
+        transaction_date__month=datetime.now().month,
+    ).order_by('-value')[:3]
+
+    bigger_transactions_of_month = [
+        {
+            'type': transaction.type,
+            'description': transaction.description,
+            'value': transaction.value,
+            'account': transaction.account.name,
+        }
+        for transaction in bigger_transactions_of_month
+    ]
 
     return render(
         request,
@@ -17,6 +32,14 @@ def app(request):
             'user_first_name': user.first_name,
             'total_balance': total_balance,
             'accounts': accounts,
-            'transactions': transactions,
+            'bigger_transactions_of_month': bigger_transactions_of_month,
         },
     )
+
+
+def signup(request):
+    return render(request, 'pages/app/signup.html')
+
+
+def login(request):
+    return render(request, 'pages/app/login.html')
