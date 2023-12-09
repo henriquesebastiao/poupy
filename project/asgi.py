4e998ec95b7230aac9dc374cb62f58be9dfa8f4f
Henrique Sebastião <contato@henriquesebastiao.com>
2023-12-08 22:53:46 -0400

    🔧 chore: Configuring project for deployment

--- project/asgi.py
+++ project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for project project.
+ASGI config for project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- project/wsgi.py
+++ project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project project.
+WSGI config for project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
