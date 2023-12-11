fea2fdbbe1cdcefb3ac60a012cc0aacca6c23bfd
Henrique Sebastião <contato@henriquesebastiao.com>
2023-12-11 15:10:04 -0400

    🧱 ci: Adding test Django to CI

--- project/settings.py
+++ project/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': os.getenv('DATABASE_ENGINE'),
         'NAME': os.getenv('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
