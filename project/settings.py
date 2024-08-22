394907e3b18a0e4975b2071d099cdfd82db59f07
Henrique Sebastião <contato@henriquesebastiao.com>
2024-08-22 08:52:36 -0400

    🔧 fix: ALLOWED_HOSTS settings

--- project/settings.py
+++ project/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
-from decouple import config
+from decouple import config, Csv
 from django.contrib.messages import constants
 from django.core.management.utils import get_random_secret_key
 
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 DEBUG = config('DEBUG', default=False, cast=bool)
 SQLITE = config('SQLITE', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'server.henriquesebastiao.com']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
 
 
 # Application definition
