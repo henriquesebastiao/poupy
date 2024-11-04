1d45abdb66eeaf07273fc1421cc8bda2c376d46f
Henrique Sebastião <contato@henriquesebastiao.com>
2024-11-04 17:44:58 -0400

    🧪 fix: Fix CSRF settings for deploy

--- project/settings.py
+++ project/settings.py
@@ -53,12 +53,12 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -175,4 +175,4 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
-CSRF_TRUSTED_ORIGINS = ['http://poupy.henriquesebastiao.com']
+CSRF_TRUSTED_ORIGINS = ['https://poupy.henriquesebastiao.com']
